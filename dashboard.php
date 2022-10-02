5f171c8e3dd78aa5796997dcde56f55c6c66403b
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2022-10-02 21:35:38 +0300

    Design structure

--- dashboard.php
+++ dashboard.php
@@ -4,8 +4,50 @@
     <meta charset="utf-8"/>
     <title>Zat Academy</title>
     <link rel="icon" href="icon.png"/>
+    
+    <style>
+        .SchoolClass
+        {    
+            padding: 20px;border: 1px dotted #000;
+            margin: 10px; display: inline-block;
+        }
+
+        button
+        {
+            height:90px;
+            width:600px;
+        }
+    </style>
 </head>
 <body>
+    <div style="text-align: center;">
+        <h1>Zat Academy Dashboard</h1>
+        <hr/>
+        <br/>
+        <br/>
+        <p> Write The New Link</p>
+        <input type="text" style="padding: 30px;width:45%"/>
 
+        <br/><br/> <br/><br/>
+        <p>Select The Classes :</p>
+        <br/>
+        <span class="SchoolClass">Class E : 1</span>
+        <span class="SchoolClass">Class E : 2</span>
+        <span class="SchoolClass">Class E : 3</span>
+        <span class="SchoolClass">Class E : 4</span>
+        <span class="SchoolClass">Class E : 5</span>
+        <span class="SchoolClass">Class E : 6</span>
+        <br/>
+        <span class="SchoolClass">Class M : 1</span>
+        <span class="SchoolClass">Class M : 2</span>
+        <span class="SchoolClass">Class M : 3</span>
+
+        <br/>
+
+        <span class="SchoolClass">Class H : 1</span>
+        
+        <br/><br/><br/>
+        <button>Save</button>
+    </div>
 </body>
 </html>
